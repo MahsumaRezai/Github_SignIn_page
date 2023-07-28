d9d903c255754f23301cf007c89b919f64a39f10
Masoumeh Rezai <you@example.com>
2023-07-29 02:02:38 +0430

    add some style in hae section

--- src/componets/Head/Head.js
+++ src/componets/Head/Head.js
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
-import log from './imag/gitlog.PNG'
+import log from './imag/gitlog.PNG';
+import classes from './Head.module.css'
 const Head = (props) => {
     return (
         <Fragment>
-            <div>
+            <div className={classes.head}>
                 <img alt="" src={log} />
-                <p>Sign in to GitHub</p>
+                <p className={classes.text}>Sign in to GitHub</p>
             </div>
         </Fragment>
 
