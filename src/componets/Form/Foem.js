8177de7dea0448218330ab24b44b7375e44eb6dc
Masoumeh Rezai <you@example.com>
2023-07-29 02:08:25 +0430

    add form

---
+++ src/componets/Form/Foem.js
@@ -0,0 +1,16 @@
+const Form = (props) => {
+    return (
+        <form>
+            <div>
+                <label>Username or email address</label>
+                <input></input>
+
+                <label>Password</label>
+                <input></input>
+                <button>send</button>
+            </div>
+
+        </form>
+    )
+}
+export default Form;
